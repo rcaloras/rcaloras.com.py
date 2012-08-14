18cbf8ca3c602a87c7b12d76d6f127d9a14d3829
Ryan Caloras <ryan@coolhandjuke.com>
2012-08-14 00:34:23 -0400

    a little closer

--- rcalorashomepage/views.py
+++ rcalorashomepage/views.py
@@ -3,3 +3,7 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project':'rcaloras-homepage'}
+
+@view_config(name='helloworld', renderer='templates/helloworld.pt')
+def helloworld_view(request):
+    return {'project':'rcaloras-homepage'}
