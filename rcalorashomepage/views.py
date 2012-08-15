d077bd01255f20603d688bed09c34ab364c0635b
Ryan Caloras <ryan@coolhandjuke.com>
2012-08-15 02:36:19 -0400

    renamed a few files

--- rcalorashomepage/views.py
+++ rcalorashomepage/views.py
@@ -1,9 +1,5 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/sanfran_splash.pt')
 def my_view(request):
-    return {'project':'rcaloras-homepage'}
-
-@view_config(name='helloworld', renderer='templates/helloworld.pt')
-def helloworld_view(request):
     return {'project':'rcaloras-homepage'}
